fd1a7571067140f5fd8e7439770ec5da34b46f51
JelenaM <jelena.medic@forsta.com>
2026-02-27 17:46:31 +0100

    Add & register DbContext, create & run migration

---
+++ BookAuditTrail/BookAuditTrailDbContext.cs
@@ -0,0 +1,60 @@
+﻿using BookAuditTrail.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookAuditTrail;
+
+public class BookAuditTrailDbContext : DbContext
+{
+    public BookAuditTrailDbContext(DbContextOptions<BookAuditTrailDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Book> Books => Set<Book>();
+    public DbSet<Author> Authors => Set<Author>();
+    public DbSet<BookAuditLog> BookAuditLogs => Set<BookAuditLog>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Book>(entity =>
+        {
+            entity.HasKey(b => b.Id);
+            entity.Property(b => b.Title).HasMaxLength(500).IsRequired();
+            entity.Property(b => b.ShortDescription).HasMaxLength(2000);
+            entity.Property(b => b.PublishDate).IsRequired();
+
+            entity.HasMany(b => b.Authors)
+                  .WithMany(a => a.Books)
+                  .UsingEntity(j => j.ToTable("BookAuthors"));
+
+            entity.HasMany(b => b.AuditLogs)
+                  .WithOne(a => a.Book)
+                  .HasForeignKey(a => a.BookId)
+                  .OnDelete(DeleteBehavior.Cascade);
+        });
+
+        modelBuilder.Entity<Author>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.Name).HasMaxLength(300).IsRequired();
+            entity.HasIndex(a => a.Name).IsUnique();
+        });
+
+        modelBuilder.Entity<BookAuditLog>(entity =>
+        {
+            entity.HasKey(a => a.Id);
+            entity.Property(a => a.ChangeType).HasMaxLength(50).IsRequired();
+            entity.Property(a => a.FieldName).HasMaxLength(100).IsRequired();
+            entity.Property(a => a.OldValue).HasMaxLength(2000);
+            entity.Property(a => a.NewValue).HasMaxLength(2000);
+            entity.Property(a => a.Description).HasMaxLength(2000).IsRequired();
+            entity.Property(a => a.ChangedAt).IsRequired();
+
+            entity.HasIndex(a => a.BookId);
+            entity.HasIndex(a => a.ChangedAt);
+            entity.HasIndex(a => a.ChangeType);
+        });
+    }
+}
---
+++ BookAuditTrail/Migrations/20260227161642_InitialCreate.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BookAuditTrail.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(300)", maxLength: 300, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Books",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: false),
+                    ShortDescription = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    PublishDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Books", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookAuditLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BookId = table.Column<int>(type: "int", nullable: false),
+                    ChangeType = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    FieldName = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    OldValue = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    NewValue = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: true),
+                    Description = table.Column<string>(type: "nvarchar(2000)", maxLength: 2000, nullable: false),
+                    ChangedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAuditLogs", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BookAuditLogs_Books_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BookAuthors",
+                columns: table => new
+                {
+                    AuthorsId = table.Column<int>(type: "int", nullable: false),
+                    BooksId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BookAuthors", x => new { x.AuthorsId, x.BooksId });
+                    table.ForeignKey(
+                        name: "FK_BookAuthors_Authors_AuthorsId",
+                        column: x => x.AuthorsId,
+                        principalTable: "Authors",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_BookAuthors_Books_BooksId",
+                        column: x => x.BooksId,
+                        principalTable: "Books",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Authors_Name",
+                table: "Authors",
+                column: "Name",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuditLogs_BookId",
+                table: "BookAuditLogs",
+                column: "BookId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuditLogs_ChangeType",
+                table: "BookAuditLogs",
+                column: "ChangeType");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuditLogs_ChangedAt",
+                table: "BookAuditLogs",
+                column: "ChangedAt");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BookAuthors_BooksId",
+                table: "BookAuthors",
+                column: "BooksId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "BookAuditLogs");
+
+            migrationBuilder.DropTable(
+                name: "BookAuthors");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.DropTable(
+                name: "Books");
+        }
+    }
+}
